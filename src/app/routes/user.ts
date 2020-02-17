0cc040b710b013cda72e5c14f80b1dc57d5153ba
mohamed bouaziz <mohamed_bouaziz@hotmail.fr>
2020-02-17 11:23:31 +0100

    persist data from container to local folder

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -1,9 +1,22 @@
-import express = require('express');
+import express = require("express");
 const router = express.Router();
 
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.use('/', (req, res, next) => {
+// get user by ID
+router.get("/:id", (req, res, next) => {
+  User.findOne({
+    _id: req.params.id
+  })
+    .then(user => {
+      console.log(user);
+      res.status(200).json(user);
+    })
+    .catch(error => res.status(404).json({ error: error }));
+});
+
+// get all users
+router.use("/", (req, res, next) => {
   User.find()
     .then(users => res.status(200).json(users))
     .catch(error => res.status(400).json({ error }));
