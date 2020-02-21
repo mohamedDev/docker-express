0088e49b4a327e1572215b84c76d6ab04e87f33e
mohamed bouaziz <mohamed.bouaziz@sifast.com>
2020-02-21 17:43:03 +0100

    add jsdoc to express socle

--- src/app/user/user.route.ts
+++ src/app/user/user.route.ts
@@ -6,6 +6,29 @@ let userRoutes = express.Router();
 let usersController = new UsersController();
 
 userRoutes
+  /**
+ * @swagger
+ *
+ * /login:
+ *   post:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Username to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
+ */
   .get("/", usersController.index)
   .get("/:id", usersController.show)
   .post("/", usersController.create)
